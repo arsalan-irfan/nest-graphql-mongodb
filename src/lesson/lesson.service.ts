7461275564585564aba5ef8da47e05afd5c2af8d
Muhammad Arsalan <43111371+arsalan318@users.noreply.github.com>
2022-01-20 18:00:26 +0500

    Done with lesson service

---
+++ src/lesson/lesson.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Lesson } from './lesson.entity';
+import { v4 as uuid } from 'uuid';
+import { CreateLessonInput } from './lesson.input';
+@Injectable()
+export class LessonService {
+  constructor(
+    @InjectRepository(Lesson) private lessonRepository: Repository<Lesson>,
+  ) {}
+
+  async createLesson(createLessonInput: CreateLessonInput): Promise<Lesson> {
+    const { name, startDate, endDate } = createLessonInput;
+    const lesson = this.lessonRepository.create({
+      id: uuid(),
+      name,
+      startDate,
+      endDate,
+    });
+    return this.lessonRepository.save(lesson);
+  }
+  async getLesson(id: string): Promise<Lesson> {
+    return this.lessonRepository.findOne({ id });
+  }
+  getLessons(): Promise<Lesson[]> {
+    return this.lessonRepository.find();
+  }
+}
